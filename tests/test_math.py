c15305b683303b687473ba0c4d1ef4835378cffc
Seth McCombs <seth.mccombs@workday.com>
2021-01-12 15:39:49 -0800

    testing stuff

--- tests/test_math.py
+++ tests/test_math.py
@@ -13,3 +13,22 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
     assert 'division by zero' in str(e.value)
+
+# Multiplication test ideas
+
+# two positive integers
+# identityL multiplying any number by 1
+# zero: multiplying any number by zero
+# positive by a negative
+# negative by a negative
+# multiply floats
+
+def test_multiply_two_positive_ints():
+  assert 2 * 3 == 6
+
+def test_multiply_identity():
+  assert 1 * 99 == 99
+
+def test_multiply_zero():
+  assert 0 * 100 == 0
+
