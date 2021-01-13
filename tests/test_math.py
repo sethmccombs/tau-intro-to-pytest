c0caabe08dc5785efc34dac0ad9b2e8acb96e81a
Seth McCombs <seth.mccombs@workday.com>
2021-01-13 10:45:21 -0800

    more test

--- tests/test_math.py
+++ tests/test_math.py
@@ -23,12 +23,17 @@ def test_divide_by_zero():
 # negative by a negative
 # multiply floats
 
-def test_multiply_two_positive_ints():
-  assert 2 * 3 == 6
+products = [
+    (2, 3, 6),  # positive integers
+    (1, 99, 99),  # identity
+    (0, 99, 0),  # zero
+    (3, -4, -12),  # positive by negative
+    (-5, -5, 25),  # negative by negative
+    (2.5, 6.7, 16.75)  # floats
+]
 
-def test_multiply_identity():
-  assert 1 * 99 == 99
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
 
-def test_multiply_zero():
-  assert 0 * 100 == 0
 
